82214786d286af9c1c1ee940d85478a50b98955c
Lord Stoker <tpc00@iesemilidarder.com>
2025-01-05 14:49:58 +0100

    first commit

---
+++ Tenzies/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import Die from './components/Die'
+
+export default function App() {
+
+  function generateAllNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return newDice
+  }
+
+  const newDice = generateAllNewDice();
+  console.log(newDice);
+
+  return(
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={2} />
+        <Die value={5} />
+        <Die value={5} />
+        <Die value={1} />
+      </div>
+    </main>
+  )
+}
+
+
